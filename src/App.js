6c942aafc5de7e14dac1574a9fbdc69f6dc5e6dd
Ankit <128883341+Ankit-can-ctrl@users.noreply.github.com>
2023-12-15 22:18:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,27 +24,53 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setcurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setcurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+      {/* accordion components is made reuseable component using children props */}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setcurOpen}
+        title="Learn React"
+        num={4}
+        key={4}
+      >
+        <p>Allows react developers to:</p>
+        <li>Break up UI into components</li>
+        <li>Make components reuseable</li>
+        <li>Place state efficiently</li>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setisOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  // isOpen will be true if num === curOpen
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    // if already opened then it will set curOpen to null
+    onOpen(isOpen ? null : num);
+  }
 
   return (
-    <div
-      className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setisOpen(!isOpen)}
-    >
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
